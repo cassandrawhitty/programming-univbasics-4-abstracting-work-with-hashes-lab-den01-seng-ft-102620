c449a32d385fc64c55bda556b75bbf657fda63df
Cassandra Whitty <cassandraallexwhitty@gmail.com>
2020-10-14 13:18:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,10 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  { key => "value"}
 end
 
 def read_from_hash(hash, key)
+  { key => "hash"}
   # return the correct value using the hash and key parameters
 end
 
